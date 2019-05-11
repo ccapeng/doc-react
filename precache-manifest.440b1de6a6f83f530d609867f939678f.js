acac43d581fb8e2bd52cc2c9e7a2b368cb560e48
apeng <apeng@timecruiser.com>
2019-05-11 17:51:34 -0400

    Updates

---
+++ precache-manifest.440b1de6a6f83f530d609867f939678f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef121c3079f391c5ece6373b447c824",
+    "url": "/doc-react/index.html"
+  },
+  {
+    "revision": "383ccf057b7e3090b132",
+    "url": "/doc-react/static/css/main.d5a0fb6d.chunk.css"
+  },
+  {
+    "revision": "a0e5d445f533aeb934a7",
+    "url": "/doc-react/static/js/2.bd739747.chunk.js"
+  },
+  {
+    "revision": "383ccf057b7e3090b132",
+    "url": "/doc-react/static/js/main.71aee6b5.chunk.js"
+  },
+  {
+    "revision": "2691d6e8309ac15a00cb",
+    "url": "/doc-react/static/js/runtime~main.82d23f77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.efc7c19821888115f690e4cbbfe85f40.js"
+  "/doc-react/precache-manifest.440b1de6a6f83f530d609867f939678f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doc-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
