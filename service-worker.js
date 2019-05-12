880ba30f6caeba1377f8ccee1c0beb62bf54e582
apeng <apeng@timecruiser.com>
2019-05-12 11:34:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doc-react/precache-manifest.9f42b4b02c516be71a6d0fd5aa74acf9.js"
+  "/doc-react/precache-manifest.19918eea067568ccb9543c4712684069.js"
 );
 
 self.addEventListener('message', (event) => {
